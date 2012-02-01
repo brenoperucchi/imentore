ff98e84a954965450d5abdf70230d98d235ad845
Vinicius Horewicz <vinicius@horewi.cz>
2012-02-01 17:15:45 -0200

    Add database_cleaner and fix store/success w/ slim

--- imentore-store/spec/dummy/db/schema.rb
+++ imentore-store/spec/dummy/db/schema.rb
@@ -13,6 +13,41 @@
 
 ActiveRecord::Schema.define(:version => 20120131205928) do
 
+  create_table "employees", :force => true do |t|
+    t.string   "name"
+    t.string   "brand"
+    t.string   "irs_id"
+    t.date     "birthdate"
+    t.string   "national_id"
+    t.string   "gender"
+    t.string   "person_type"
+    t.integer  "store_id"
+    t.string   "department"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "stores", :force => true do |t|
+    t.string   "name"
+    t.string   "brand"
+    t.string   "url"
+    t.string   "irs_id"
+    t.boolean  "active",                :default => false
+    t.text     "settings"
+    t.string   "national_id"
+    t.string   "date_initial_business"
+    t.string   "where_meet_us"
+    t.string   "email"
+    t.string   "store_type"
+    t.integer  "plan_id"
+    t.integer  "user_agent_id"
+    t.string   "state",                 :default => "pending"
+    t.datetime "disabled_at"
+    t.datetime "actived_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
