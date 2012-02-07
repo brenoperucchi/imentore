2a969bbddce9b0f3481a56e72424be452a18a0f9
Vinicius Horewicz <vinicius@horewi.cz>
2012-02-07 18:50:17 -0200

    Extends ApplicationController to override after_sign_in_path
    Uses config.to_prepare to reload classes even if cache_classes is false.
    http://www.cowboycoded.com/2011/01/28/reloading-rails-3-engine-initializers-in-development/
    http://www.cowboycoded.com/2011/02/11/working-with-applicationcontroller-in-a-rails-3-engine/
    http://stackoverflow.com/questions/5045068/extending-controllers-of-a-rails-3-engine-in-the-main-app
    http://stackoverflow.com/questions/7719633/how-to-extend-rails-engines-controllers-properly
    http://stackoverflow.com/questions/5852580/why-do-includes-in-rails-engine-initializers-malfunction-when-cache-classes-fa
    https://github.com/spree/spree/blob/master/promo/lib/spree/promo/engine.rb
    https://github.com/resolve/refinerycms/blob/master/core/lib/refinery/core/engine.rb
    https://github.com/svenfuchs/adva-cms2/blob/develop/adva-blog/lib/adva/blog.rb

--- imentore-user/lib/imentore-user/engine.rb
+++ imentore-user/lib/imentore-user/engine.rb
@@ -1,4 +1,18 @@
+module ApplicationControllerDecorator
+  def after_sign_in_path_for(resource)
+    'http://google.com'
+  end
+end
+
 module ImentoreUser
   class Engine < Rails::Engine
+    config.to_prepare do
+      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_slice.rb')) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+    end
+    # initializer 'myengine.app_controller' do |app|
+    #   ApplicationController.send(:include, ApplicationControllerDecorator)
+    # end
   end
 end
