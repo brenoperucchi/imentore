781f145b3275be14152bd4465400a5b2eb9a8c41
Vinicius Horewicz <vinicius@horewi.cz>
2012-02-10 16:16:12 -0200

    Removes old fixtures files

--- imentore-store/spec/spec_helper.rb
+++ imentore-store/spec/spec_helper.rb
@@ -27,21 +27,10 @@ DatabaseCleaner.strategy = :truncation
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
-
   config.before(:each) do
     DatabaseCleaner.clean
     Capybara.reset_sessions!
   end
-
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/../../spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
