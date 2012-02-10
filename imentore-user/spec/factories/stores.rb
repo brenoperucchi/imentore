7838dde4a2ff9bd4b1fc3076f56674b4a5979868
Vinicius Horewicz <vinicius@horewi.cz>
2012-02-10 16:34:19 -0200

    Add core to user and update factories/stores

--- imentore-user/spec/factories/stores.rb
+++ imentore-user/spec/factories/stores.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :myshop, class: Imentore::Store do
-    name 'MyShop'
+    brand 'MyShop'
     url 'myshop'
     association :owner, factory: 'myshop_owner', method: :build
   end
