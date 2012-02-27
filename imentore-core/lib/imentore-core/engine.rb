1285b92305bc43a017459c52f12c94556fb9075b
Vinicius Horewicz <vinicius@horewi.cz>
2012-02-27 19:53:16 -0300

    Load slices on core

--- imentore-core/lib/imentore-core/engine.rb
+++ imentore-core/lib/imentore-core/engine.rb
@@ -1,4 +1,9 @@
 module ImentoreCore
   class Engine < Rails::Engine
+    config.to_prepare do
+      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_slice.rb')) do |c|
+        Rails.configuration.cache_classes ? require(c) : load(c)
+      end
+    end
   end
 end
