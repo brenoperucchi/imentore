55bc96e814abb3792973f4e7a5ca89b65c2db026
Vinicius Horewicz <vinicius@horewi.cz>
2012-02-27 20:11:30 -0300

    Updates core to use testing gem

--- imentore-core/spec/dummy/db/schema.rb
+++ imentore-core/spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120210193534) do
+ActiveRecord::Schema.define(:version => 20120210192304) do
 
   create_table "addresses", :force => true do |t|
     t.string  "name"
