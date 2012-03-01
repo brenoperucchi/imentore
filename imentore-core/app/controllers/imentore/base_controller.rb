c1cf2b75a1b84cf17cd51cc893f27abd9900118b
Breno Perucchi <bperucchi@gmail.com>
2012-03-01 20:01:45 -0300

    Move Filter of application to BaseController [touch: 24]

--- imentore-core/app/controllers/imentore/base_controller.rb
+++ imentore-core/app/controllers/imentore/base_controller.rb
@@ -1,4 +1,19 @@
 module Imentore
   class BaseController < ApplicationController
+  	before_filter :check_store
+
+  	helper_method :current_store
+
+  	def current_store
+  	  @current_store ||= Imentore::Store.find_by_url(request.subdomain)
+  	end
+
+  	def check_store
+  	  unless current_store
+  	    render('shared/not_found', status: 404)
+  	    return false
+  	  end
+  	end
+  	
   end
 end
