b9926b34d2f74bd98a6e3037c85d44b305e90e47
Vinicius Horewicz <vinicius@horewi.cz>
2012-03-15 11:40:13 -0300

    Fix engine name

--- imentore-ecommerce/lib/imentore-ecommerce/engine.rb
+++ imentore-ecommerce/lib/imentore-ecommerce/engine.rb
@@ -2,7 +2,7 @@ module Imentore
   module Ecommerce
     class Engine < Rails::Engine
       isolate_namespace Imentore
-      engine_name "imentore_store"
+      engine_name "imentore_ecommerce"
 
       config.to_prepare do
         Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_slice.rb')) do |c|
