ab4d94b7210ad9920687917c914f78ac8c88b69b
Vinicius Horewicz <vinicius@horewi.cz>
2012-03-15 11:42:33 -0300

    Fix domains routing specs

--- imentore-ecommerce/spec/routing/imentore/admin/domains_routing_spec.rb
+++ imentore-ecommerce/spec/routing/imentore/admin/domains_routing_spec.rb
@@ -1,15 +1,17 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe "Admin::Domains routes" do
+  before { @routes = Imentore::Core::Engine.routes }
+
 	it "GET /admin/domains to admin/domains#new" do
-		{ get: '/admin/domains'}.should route_to(:controller => 'imentore/admin/domains', action: 'index' )
+		get("/admin/domains").should route_to("imentore/admin/domains#index")
 	end
 
 	it "POST /admin/domains to admin/domains#create" do
-		{ post: '/admin/domains'}.should route_to(:controller => 'imentore/admin/domains', action: 'create' )
+		post("/admin/domains").should route_to("imentore/admin/domains#create")
 	end
 
 	it "DELETE /admin/domains/1 to admin/domains#destroy" do
-		{ delete: '/admin/domains/1'}.should route_to(:controller => 'imentore/admin/domains', action: 'destroy', id: '1')
+		delete("/admin/domains/1").should route_to("imentore/admin/domains#destroy", id: "1")
 	end
 end
