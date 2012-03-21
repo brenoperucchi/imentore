36b0c998fd662bcf2050f2189a61deb13261a762
Breno Perucchi <breno@imentore.com.br>
2012-03-21 12:51:19 -0300

    Migrations of domain model

--- imentore-ecommerce/db/migrate/20120302231906_create_domains.rb
+++ imentore-ecommerce/db/migrate/20120302231906_create_domains.rb
@@ -1,13 +1,10 @@
 class CreateDomains < ActiveRecord::Migration
-  def up
-    create_table(:imentore_domains) do |t|
+  def change
+    create_table :domains do |t|
     	t.string :name
     	t.references :store
+
       t.timestamps
     end
-  end
-
-  def down
-    remove_table(:imentore_domains)
   end
 end
