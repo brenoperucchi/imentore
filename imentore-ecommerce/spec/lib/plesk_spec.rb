41295fe54265f42ccf6d3d9488b1ec3132b1a11b
Breno Perucchi <bperucchi@gmail.com>
2012-03-27 23:23:58 -0300

    completed plesk_spec and model (updated #2)

--- imentore-ecommerce/spec/lib/plesk_spec.rb
+++ imentore-ecommerce/spec/lib/plesk_spec.rb
@@ -15,52 +15,52 @@ describe Imentore::Plesk do
   describe "#add_domain" do
     it "raises PleskError when connection refused error" do
       plesk.host = "http://imentore.com.br:22"
-      expect { plesk.add_domain }.to raise_error(Imentore::PleskError)
+      expect { plesk.add_domain }.to raise_error(Imentore::PleskException)
     end
 
     it "raises PleskError when DNS error" do
       plesk.host = "http://nao-existe.co"
-      expect { plesk.add_domain }.to raise_error(Imentore::PleskError)
+      expect { plesk.add_domain }.to raise_error(Imentore::PleskException)
     end
 
     it "raises PleskError on timeout" do
       plesk.host = "http://imentore.com.br"
       plesk.timeout = 0.3
-      expect { plesk.add_domain }.to raise_error(Imentore::PleskError)
+      expect { plesk.add_domain }.to raise_error(Imentore::PleskException)
     end
 
     # it "raises PleskError when whatever other error" do
     #   plesk.host = "http://imentore.com.br"
     #   plesk.timeout = 1
-    #   expect { plesk.add_domain }.to raise_error(Imentore::PleskError)
+    #   expect { plesk.add_domain }.to raise_error(Imentore::PleskException)
     # end
 
     it "raises PleskError on API system error" do
       plesk.host = "http://imentore.com.br"
       plesk.rpc_version = '1.40.2.0'
-      expect { plesk.add_domain }.to raise_error(Imentore::PleskError)
+      expect { plesk.add_domain }.to raise_error(Imentore::PleskException)
     end
 
     # it "raises PleskError on agent error" do
     #   plesk.host = "imentore.com.br"
     #   plesk.rpc_version = '1.6.0.2'
-    #   expect { plesk.add_domain }.to raise_error(Imentore::PleskError)
+    #   expect { plesk.add_domain }.to raise_error(Imentore::PleskException)
     # end
 
     it "raises PleskError on wrong username" do
       plesk.user = "test"
       plesk.host = 'imentore.com.br'
-      expect { plesk.add_domain }.to raise_error(Imentore::PleskError)
+      expect { plesk.add_domain }.to raise_error(Imentore::PleskException)
     end
 
     it "raises PleskError on wrong password" do
       plesk.pass = "123"
       plesk.host = 'imentore.com.br'
-      expect { plesk.add_domain}.to raise_error(Imentore::PleskError)
+      expect { plesk.add_domain}.to raise_error(Imentore::PleskException)
     end
 
     it "raises PleskError on domain already exist" do
-      expect { plesk.add_domain("imentore.com.br")}.to raise_error(Imentore::PleskError)
+      expect { plesk.add_domain("imentore.com.br")}.to raise_error(Imentore::PleskException)
     end
 
     it "can add a domain" do
@@ -68,21 +68,35 @@ describe Imentore::Plesk do
       @@result.status.should be  == "ok"
     end
 
-    it "can add mail on domain" do
+    it "can add mail account on domain" do
       result_mail = plesk.add_mail_domain(@@result.plesk_id, mail_name, mail_password)
       result_mail.status.should be  == "ok"
     end
 
-    it "can remove mail on domain" do
+    it "'can change mail account password'" do
+      result_mail = plesk.change_mail_domain(@@result.plesk_id, mail_name, "321321")
+      result_mail.status.should be  == "ok"
+    end
+
+    it "can remove mail account on domain" do
       result_mail = plesk.del_mail_domain(@@result.plesk_id, mail_name)
       result_mail.status.should be  == "ok"
+    end
+
+    it "can be enable mail domain " do
+      result_mail = plesk.enable_mail_domain(@@result.plesk_id)
+      result_mail.status.should be  == "ok"      
+    end
+
+    it "can be disable mail domain " do
+      result_mail = plesk.disable_mail_domain(@@result.plesk_id)
+      result_mail.status.should be  == "ok"      
     end
 
     it "can remove domain" do
       @@result = plesk.del_domain(@@result.plesk_id)
       @@result.status.should be == "ok"
     end
-
 
   end
 end
