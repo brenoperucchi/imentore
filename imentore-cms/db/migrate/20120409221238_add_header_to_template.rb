627f8bc4057da2ff9063e8103f7129501afdd8b4
Breno Perucchi <breno@imentore.com.br>
2012-04-10 20:30:48 -0300

    Remover head column

--- imentore-cms/db/migrate/20120409221238_add_header_to_template.rb
+++
@@ -1,9 +0,0 @@
-class AddHeaderToTemplate < ActiveRecord::Migration
-  def up
-    add_column(:imentore_templates, :head, :text)
-  end
-
-  def down
-    remove_column(:imentore_themes, :head)
-  end
-end
