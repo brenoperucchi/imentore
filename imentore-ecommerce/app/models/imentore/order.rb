3bde4021d876877377b013be68667eee32943cd1
Vinicius Horewicz <vinicius@horewi.cz>
2012-04-11 12:38:16 -0300

    Update Order w/ new LineItem

--- imentore-ecommerce/app/models/imentore/order.rb
+++ imentore-ecommerce/app/models/imentore/order.rb
@@ -1,4 +1,21 @@
 module Imentore
   class Order < ActiveRecord::Base
+    attr_accessor :delivery
+    serialize :items, Array
+
+    has_one     :invoice
+    belongs_to  :store
+
+    def total_amount
+      items.sum(&:price)
+    end
+
+    def chargeable?
+      total_amount > 0
+    end
+
+    def deliverable?
+      items.map(&:deliverable?).inject(:&)
+    end
   end
 end
--- imentore-ecommerce/spec/models/imentore/order_spec.rb
+++ imentore-ecommerce/spec/models/imentore/order_spec.rb
@@ -13,19 +13,14 @@ describe Imentore::Order do
     order.billing_address.street.should eq("123 Test St.")
   end
 
-  it "has a total amount" do
-    order.total_amount = 123
-    order.total_amount.should eq(123)
-  end
-
   it "has a status" do
     order.status = "open"
     order.status.should eq("open")
   end
 
-  it "has a buyer email" do
-    order.buyer_email = "foo@bar.com"
-    order.buyer_email.should eq("foo@bar.com")
+  it "has a customer email" do
+    order.customer_email = "foo@bar.com"
+    order.customer_email.should eq("foo@bar.com")
   end
 
   it "has items" do
@@ -34,12 +29,49 @@ describe Imentore::Order do
   end
 
   it "has an invoice" do
-    order.invoice = "invoice"
-    order.invoice.should eq("invoice")
+    order.should have_one(:invoice)
   end
 
-  it "has a shipment" do
-    order.shipment = "shipment"
-    order.shipment.should eq("shipment")
+  it "has a delivery" do
+    order.delivery = "delivery"
+    order.delivery.should eq("delivery")
+  end
+
+  it "has a store" do
+    order.should belong_to(:store)
+  end
+
+  describe "#total_amount" do
+    it "is the sum of its items prices" do
+      order.stub(items: [mock(:product, price: 12), mock(:product, price: 18)])
+
+      order.total_amount.should eq(30)
+    end
+  end
+
+  describe "#chargeable?" do
+    it "is true when total amount greater than zero" do
+      order.stub(total_amount: 12)
+      order.chargeable?.should be_true
+    end
+
+    it "is false when total amount not greater than zero" do
+      order.stub(total_amount: 0)
+      order.chargeable?.should be_false
+    end
+  end
+
+  describe "#deliverable?" do
+    it "is true when all items are deliverable" do
+      order.stub(items: [mock(:product, deliverable?: true)])
+
+      order.deliverable?.should be_true
+    end
+
+    it "is false when one or more items aren't deliverable" do
+      order.stub(items: [mock(:product, deliverable?: true), mock(:product, deliverable?: false)])
+
+      order.deliverable?.should be_false
+    end
   end
 end
