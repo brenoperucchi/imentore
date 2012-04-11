0152e52b8079179231ae2241a3be617185f65f40
Breno Perucchi <breno@imentore.com.br>
2012-04-11 20:06:28 -0300

    Asset cannot belongs_to store

--- imentore-cms/app/models/imentore/asset.rb
+++ imentore-cms/app/models/imentore/asset.rb
@@ -1,10 +1,10 @@
 module Imentore
   class Asset < ActiveRecord::Base
     belongs_to :theme
-    belongs_to :store
 
     attr_accessor :filename
     # validates_uniqueness_of :file, :on => :create, :message => "must be unique", :if => proc { |obj| binding.pry; obj.class.find(:all, :conditions=> ['store_id == ?, and name == ?', obj.store_id, obj.file]).size >= 1 }
     mount_uploader :file, FileUploader
+    # validates_uniqueness_of :attribute, :on => :create, :scope => [:store_id, :tbeme_id]
   end
 end
