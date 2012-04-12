00d22d52665b6b9043323c4476f6f69ccbf5d928
Vinicius Horewicz <vinicius@horewi.cz>
2012-04-12 16:24:53 -0300

    Refactor base helper tests

--- imentore-core/spec/lib/base_helper_spec.rb
+++ imentore-core/spec/lib/base_helper_spec.rb
@@ -1,45 +1,23 @@
 require "spec_helper"
-require "imentore-core/base_helper"
-
-module Imentore
-
-  class Request
-
-    attr_accessor :domain, :subdomain
-    def initialize
-      @domain = "imentore.com"
-      @subdomain = "myshop"
-    end
-  end
-
-  class Includer < BaseController
-
-    def initialize
-      @request = Imentore::Request.new
-    end
-
-    def request
-      @request
-    end
-  end
-
-end
 
 describe 'Imentore::BaseHelper' do
+  let(:controller) { Imentore::BaseController.new }
+
   before(:each) do
     Factory.create(:myshop)
   end
 
   it "current_store" do
-    i = Imentore::Includer.new
-    i.request.domain = "another-domain.com"
-    i.current_store.should be_nil
+    controller.stub_chain(:request, :domain).and_return("another-domain.com")
+
+    controller.current_store.should be_nil
   end
 
   it "current_store be exist" do
-    i = Imentore::Includer.new
-    i.request.domain = "imentore.com"
-    i.current_store.should_not be_nil
+    controller.stub_chain(:request, :domain).and_return("imentore.com")
+    controller.stub_chain(:request, :subdomain).and_return("myshop")
+
+    controller.current_store.should be_a(Imentore::Store)
   end
 
 end
