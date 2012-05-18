6bf330e6f7b8911b1808df7c9b5d9c417fcb73a1
Breno Perucchi <breno@imentore.com.br>
2012-05-18 20:25:57 -0300

    Customer store back to checkout when unauthenticate

--- imentore-auth/app/controllers/imentore/client/sessions_controller.rb
+++ imentore-auth/app/controllers/imentore/client/sessions_controller.rb
@@ -4,10 +4,11 @@ module Imentore
       layout "client"
 
       def after_sign_in_path_for(resource)
-        if session[:user_return_to].include?('checkout')
+        if session[:user_return_to] and session[:user_return_to].include?('checkout')
           checkout_path
+        else
+          resource.userable.owner? ? admin_dashboard_path : client_dashboard_path
         end
-        resource.userable.owner? ? admin_dashboard_path : client_dashboard_path
       end
 
     end
