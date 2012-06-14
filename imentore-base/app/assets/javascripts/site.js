b2856dbbaee9827f39200376cfa73af642a029a7
Breno Perucchi <breno@imentore.com.br>
2012-06-14 03:51:58 -0300

    css

--- imentore-base/app/assets/javascripts/site.js
+++ imentore-base/app/assets/javascripts/site.js
@@ -7,4 +7,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require imentore-cms
+//= require site_index
+//= require site_jquery.colorbox-min
---
+++ imentore-base/app/assets/javascripts/site/site_index.js
@@ -0,0 +1,11 @@
+(function() {
+
+  $(function() {
+    return $(".youtube").colorbox({
+      iframe: true,
+      innerWidth: 560,
+      innerHeight: 315
+    });
+  });
+
+}).call(this);
