fdc9af45ba8b5e53094be67c2cb5643e6c12c165
Breno Perucchi <breno@imentore.com.br>
2012-08-01 20:49:33 -0300

    template updated with cache

--- imentore-cms/app/models/imentore/template.rb
+++ imentore-cms/app/models/imentore/template.rb
@@ -6,5 +6,11 @@ module Imentore
 
     validates :default, uniqueness: { scope: :theme_id }, :if => Proc.new { |klass| klass.default and klass.kind == 'layout'}
 
+    after_save :template_cached
+
+    def template_cached
+      SqlTemplate::Resolver.instance.clear_cache
+    end
+
   end
 end
