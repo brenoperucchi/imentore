3552654fb458ceed5193bf88677b0d29aafb64cf
Breno Perucchi <breno@imentore.com.br>
2012-10-09 19:35:58 -0300

    disable observers

--- imentore-base/app/models/customer_observer.rb
+++ imentore-base/app/models/customer_observer.rb
@@ -1,13 +1,13 @@
-# class CustomerObserver < ActiveRecord::Observer
-#   observe Imentore::Customer
+class CustomerObserver < ActiveRecord::Observer
+  observe Imentore::Customer
 
-#   def after_create(klass)
-#     store = klass.store
-#     send_email = store.send_emails.find_by_name('customer_create')
-#     if send_email.active?
-#       body = send_email.prepare_body_customer(klass)
-#       Imentore::SendEmailMailer.send_mail_mailer(store.email_contact,
-#                                                  klass.user.email, send_email.subject, body).deliver
-#     end
-#   end
-# end
+  def after_create(klass)
+    store = klass.store
+    send_email = store.send_emails.find_by_name('customer_create')
+    if send_email.active?
+      body = send_email.prepare_body_customer(klass)
+      Imentore::SendEmailMailer.send_mail_mailer(store.email_contact,
+                                                 klass.user.email, send_email.subject, body).deliver
+    end
+  end
+end
