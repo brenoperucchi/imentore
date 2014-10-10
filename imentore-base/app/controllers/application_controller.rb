28b6fba2c5b54205d3695b284cb6da3a2192585b
Breno Perucchi <bperucchi@gmail.com>
2014-10-10 17:38:46 -0300

    Add Info about remote ip and url

--- imentore-base/app/controllers/application_controller.rb
+++ imentore-base/app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  after_filter :logger
+
+  def logger
+    Rails.logger.info { "Remote  IP : #{request.remote_ip}" }
+    Rails.logger.info { "Request URL: #{request.referrer}" }
+  end
+
 end
