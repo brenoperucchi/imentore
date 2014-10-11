91cc25f2e9c3b93cac932832c7cba708e558f5bb
Breno Perucchi <bperucchi@gmail.com>
2014-10-11 16:26:18 -0300

    user update on admin

--- imentore-auth/app/models/imentore/user.rb
+++ imentore-auth/app/models/imentore/user.rb
@@ -30,7 +30,7 @@ module Imentore
     # validates_length_of :password, :within => Devise.password_length, :allow_blank => true, :if => :password_required
     
     validates :email, uniqueness: { :case_sensitive => false, :allow_blank => true, scope: [:email, :store_id] }, :if => :email_changed?    
-    validates :password, :email, presence: true
+    validates :password, :email, presence: true, on: :create
     validates :password, confirmation: true
 
 
