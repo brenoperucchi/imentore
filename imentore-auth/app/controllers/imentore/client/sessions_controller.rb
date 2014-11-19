c7d58281225beae8affd41ca3e642f0ad763653b
Breno Perucchi <bperucchi@gmail.com>
2014-11-18 22:40:30 -0200

    cart -> checkout bug fix

--- imentore-auth/app/controllers/imentore/client/sessions_controller.rb
+++ imentore-auth/app/controllers/imentore/client/sessions_controller.rb
@@ -11,7 +11,7 @@ module Imentore
 
       def after_sign_in_path_for(resource)
         if session[:user_return_to] and session[:user_return_to].include?('checkout')
-          checkout_path
+          address_checkout_path
         else
           (resource.userable.owner? || resource.userable.admin?) ? admin_dashboard_path : client_dashboard_path
         end
