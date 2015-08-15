90d3fd72f988ff79ee083a3307bd195b0b5fcb78
Breno Perucchi <bperucchi@gmail.com>
2015-08-15 17:08:00 -0300

    fix template

--- imentore-cms/app/models/imentore/template.rb
+++ imentore-cms/app/models/imentore/template.rb
@@ -7,7 +7,7 @@ module Imentore
     scope :templates, -> { where(kind: 'template') }
     belongs_to :theme
     belongs_to :layout, :class_name => Imentore::Template, :foreign_key => "layout_id"
-    # belongs_to :admin_template, :class_name => AdminImentore::Template, :foreign_key => "admin_imentore_template_id"
+    belongs_to :manager_template, :class_name => Imentore::Manager::Template, :foreign_key => "admin_imentore_template_id"
 
     # def layout
     #   self.read_attribute(:layout).present? ? self.read_attribute(:layout) : nil
