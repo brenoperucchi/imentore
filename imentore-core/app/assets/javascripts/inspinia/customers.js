df3289aa80983e4f58b4094b8ada44bd8cc2c6d5
Breno Perucchi <bperucchi@gmail.com>
2015-10-23 01:17:44 -0200

    fixings admin views

---
+++ imentore-core/app/assets/javascripts/inspinia/customers.js
@@ -0,0 +1,4 @@
+//= require inspinia/dataTables/jquery.dataTables.js
+//= require inspinia/dataTables/dataTables.bootstrap.js
+//= require inspinia/dataTables/dataTables.responsive.js
+//= require inspinia/dataTables/dataTables.tableTools.min.js
