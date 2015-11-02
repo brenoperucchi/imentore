76b58e8dce37cd7dd55726a1e5db2ba61c62ae6d
Breno Perucchi <bperucchi@gmail.com>
2015-11-01 23:05:12 -0200

    fix products images

--- imentore-ecommerce/app/controllers/imentore/admin/images_controller.rb
+++ imentore-ecommerce/app/controllers/imentore/admin/images_controller.rb
@@ -5,11 +5,12 @@ module Imentore
       inherit_resources
       belongs_to :variant, parent_class: Imentore::ProductVariant
       skip_before_action :verify_authenticity_token, :if => Proc.new {|c| c.request.format == 'application/json'}
-
       respond_to :json, only: [:create, :index, :destroy, :new]
+      respond_to :js, only: [:index, :destroy]
 
       def index
-        respond_with(files: collection.map {|image| Imentore::ImagePresenter.new(image).to_json})
+        # respond_with(files: collection.map {|image| Imentore::ImagePresenter.new(image).to_json})
+        index!
       end
 
       def create
@@ -22,6 +23,13 @@ module Imentore
 
       def image_params
         params.require(:image).permit(:picture)
+      end
+
+      def destroy
+        destroy! do |format|
+          format.html { redirect_to admin_product_variant_images_path(@variant.product, @variant) }
+          format.js { @images = collection }
+        end
       end
     
     end
